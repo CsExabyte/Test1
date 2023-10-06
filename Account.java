0436416eaf21f4d32a2bc450f5fcec8afd5fb56a
CsExabyte <147121949+CsExabyte@users.noreply.github.com>
2023-10-06 08:55:47 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -11,6 +11,7 @@ Account(int acc_no,int balance,String name,String acc_type)
 	this.acc_type=acc_type;
 	
 }
+	//ganggananggg
 void display() 
 	{
 		System.out.println("Accno: "+acc_no+" Acctype: "+acc_type+" Name: "+name+" Balance: "+balance);
